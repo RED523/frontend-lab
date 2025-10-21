3b2d1dbaceba574604e1968457f30024fbe6cf36
zhaozhongming <coderzzm@foxmail.com>
2025-10-21 22:26:27 +0800

    fix: 更新package.json中的作者名称，并调整vite.config.js中的基础路径处理

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
+  const base = command === 'build' ? '/frontend-lab/' : '/'
   const demoPath = process.env.DEMO_PATH
 
   // 规范化 open 路径：
   // - 若传入的是文件（含扩展名），按文件打开
   // - 若传入的是目录，则追加 /index.html
   const normalizedOpen = (() => {
-    if (!demoPath) return true
-    const cleaned = demoPath.replace(/^\/+/, '')
-    const hasExt = /\.[a-zA-Z0-9]+$/.test(cleaned)
-    const target = hasExt ? cleaned : `${cleaned.replace(/\/+$/, '')}/index.html`
-    return `/${target}`
+    if (!demoPath) return true // 默认打开浏览器首页
+    const cleaned = demoPath.replace(/^\/+/, '') // 去掉开头的 /，即：'/vue-examples/vue3-reactive' → 'vue-examples/vue3-reactive'
+    const hasExt = /\.[a-zA-Z0-9]+$/.test(cleaned) // 判断是文件还是目录。文件一般会有扩展名，如 .html、.htm、.php 等等
+    const target = hasExt ? cleaned : `${cleaned.replace(/\/+$/, '')}/index.html` // 若为文件则直接使用，否则是目录，去掉目录结尾的/，然后在末尾加上 /index.html
+    return `${base}${target}` // 拼接 base 路径返回完整路径
   })()
 
   return {
     plugins: [vue(), react()],
-    base: '/frontend-lab/',
+    base,
     server: {
       port: 3000,
       open: normalizedOpen
